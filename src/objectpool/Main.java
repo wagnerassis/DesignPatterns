d2584276363f5f99a2a36bdcb4fe29d8565604fe
Wagner Assis <wagner.czb@gmail.com>
2017-09-15 17:41:25 -0300

    object pool pattern

---
+++ src/objectpool/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectpool;
+
+/**
+ *
+ * @author https://sourcemaking.com/design_patterns/object_pool/java
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    
+//// Create the ConnectionPool:
+//    JDBCConnectionPool pool = new JDBCConnectionPool(
+//      "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+//      "sa", "secret");
+//
+//    // Get a connection:
+//    Connection con = pool.checkOut();
+//
+//    // Use the connection
+//    ...
+//
+//    // Return the connection:
+//    pool.checkIn(con);
+//    
+}
+}
